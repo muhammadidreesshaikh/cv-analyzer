ce5f8637c78d880d7885b44cf3d63d9d3d0b2a54
muhammadidreesshaikh <idreesiqbal104@gmail.com>
2021-11-18 23:59:09 +0500

    companylisting page done

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -23,10 +23,32 @@ function Header() {
                                     <li><Link to="/home">Home</Link></li>
                                     <li><Link to="/services">Services</Link></li>
                                     <li><Link to="/about">About</Link></li>
-                                    <li><Link to="/admin">Admin</Link></li>
-                                    <li><Link to="/createcompany">Create Company</Link></li>
-                                    <li><Link to="/applyjob">Apply Job</Link></li>
-                                    <li><Link to="/createjob">Create Job</Link></li>
+
+                                    <div className="dropdown">
+                                        <button class="dropbtn">Admin</button>
+                                        <div class="dropdown-content">
+                                            <Link to="/allcompanies">All Companies</Link>
+                                            <Link to="/alljobs">All Jobs</Link>
+                                        </div>
+                                    </div>
+
+                                    <div className="dropdown">
+                                        <button class="dropbtn">Companies</button>
+                                        <div class="dropdown-content">
+                                            <Link to="/createcompany">Create Company</Link>
+                                            <Link to="/companylisting">Company Listing</Link>
+                                        </div>
+                                    </div>
+
+                                    <div className="dropdown">
+                                        <button class="dropbtn">Jobs</button>
+                                        <div class="dropdown-content">
+                                            <Link to="/createjob">Create Job</Link>
+                                            <Link to="/applyjob">Apply Job</Link>
+                                            <Link to="/joblisting">Job Listing</Link>
+                                        </div>
+                                    </div>
+
                                     <li><Link to="/contact">Contact</Link></li>
                                     <li><Link to="/login">Login</Link></li>
                                  </ul>
--- src/pages/company/Companylisting.js
+++ src/pages/company/Companylisting.js
@@ -1,5 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../assets/css/companylisting.css";
+
+// image
+import image from "../../assets/img/company.jpg";
+import image1 from "../../assets/img/company1.jpg";
+import image2 from "../../assets/img/company2.jpg";
+import image3 from "../../assets/img/company3.jpg";
+import image4 from "../../assets/img/company4.jpg";
+import image5 from "../../assets/img/company5.jpg";
+import image6 from "../../assets/img/company.6.jpeg";
+
+
+// arary
+let Data = [
+  {
+    id: "1",
+    image: image,
+    name: "Tech Soltution",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "2",
+    image: image1,
+    name: "Nature Tech",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "3",
+    image: image2,
+    name: "Contour Software House",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "4",
+    image: image3,
+    name: "Xpert Soltution ",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "5",
+    image: image4,
+    name: "Aim Soltution",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "6",
+    image: image5,
+    name: "Contour Soltution",
+    location: "Karachi, Pakistan",
+  },
+  {
+    id: "7",
+    image: image6,
+    name: "Makeen Software House",
+    location: "Karachi, Pakistan",
+  },
+];
 
 class Companylisting extends React.Component {
   constructor(props) {
@@ -24,11 +81,27 @@ class Companylisting extends React.Component {
       <div>
         <div className="company-listing">
           <div className="container">
-            <div className="row justify-content-center">
+            <div className="row">
 
-                <div className="col-12">
-                  <p>company-listing</p>
-                </div>
+              {Data.map((item, i) => {
+                return (
+
+                  <div className="col-md-4" key={i}>
+                    <div className="card">
+                      <img src={item.image} />
+
+                      <div className="card-body">
+                        <h5>{item.name}</h5>
+                        <p>{item.location}</p>
+                        <div className="company-btn">
+                          <Link to="/menu" className="btn btn-primary">View All Companies</Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                );
+              })}
 
             </div>
           </div>
