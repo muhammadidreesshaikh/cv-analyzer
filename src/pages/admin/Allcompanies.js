9f43dafee4fd3e1371d19c994a4af02aa2bba85a
Muhammad Idrees <47222718+muhammadidreesshaikh@users.noreply.github.com>
2022-01-03 20:47:35 +0500

    class component to convert in fuction component

--- src/pages/admin/Allcompanies.js
+++ src/pages/admin/Allcompanies.js
@@ -1,133 +1,115 @@
 import React from "react";
 import "../../assets/css/all-companies.css";
 
-class AllCompanies extends React.Component {
-  constructor(props) {
-    super(props);
+function AllCompanies() {
 
-    this.state = {
-      data: [],
-      loading: false,
-    };
-  }
+  return (
+    <div>
+      <div className="all-companies">
+        <div className="container">
+          <div className="row justify-content-center">
 
-  componentDidMount() {
-    console.log("All Companies");
+              <div className="col-12">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>No.</th>
+                      <th>Company</th>
+                      <th>Owner</th>
+                      <th>Email</th>
+                      <th>Website</th>
+                      <th>Number</th>
+                      <th>City</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Sample</td>
+                      <td>Jacky</td>
+                      <td>adomjacky@gmail.com</td>                       
+                      <td>www.sample.com</td>
+                      <td>+1334991276</td>
+                      <td>Chicago</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Dummy</td>
+                      <td>Semth</td>
+                      <td>johnsemth@gmail.com</td>
+                      <td>www.dummy.com</td>
+                      <td>+1300277811</td>                       
+                      <td>Houston</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Solution</td>
+                      <td>Jorg</td>
+                      <td>ancy@gmail.com</td>
+                      <td>www.solution.com</td>
+                      <td>+1233003487</td>
+                      <td>Philadelphia</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Teck</td>
+                      <td>Amy</td>
+                      <td>rocky@gmail.com</td>
+                      <td>www.teck.com</td>
+                      <td>+1800158813</td>
+                      <td>Phoenix</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>Dummy</td>
+                      <td>Semth</td>
+                      <td>johnsemth@gmail.com</td>
+                      <td>www.dummy.com</td>
+                      <td>+1300277811</td>                       
+                      <td>Houston</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>6</td>
+                      <td>Sample</td>
+                      <td>Jacky</td>
+                      <td>adomjacky@gmail.com</td>                       
+                      <td>www.sample.com</td>
+                      <td>+1334991276</td>
+                      <td>Chicago</td>
+                      <td>
+                        <a className="far fa-edit mr-4 text-success"></a>
+                        <a className="fas fa-trash-alt text-danger"></a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
 
-    this.setState({
-      loading: true,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="all-companies">
-          <div className="container">
-            <div className="row justify-content-center">
-
-                <div className="col-12">
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>No.</th>
-                        <th>Company</th>
-                        <th>Owner</th>
-                        <th>Email</th>
-                        <th>Website</th>
-                        <th>Number</th>
-                        <th>City</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Sample</td>
-                        <td>Jacky</td>
-                        <td>adomjacky@gmail.com</td>                       
-                        <td>www.sample.com</td>
-                        <td>+1334991276</td>
-                        <td>Chicago</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Dummy</td>
-                        <td>Semth</td>
-                        <td>johnsemth@gmail.com</td>
-                        <td>www.dummy.com</td>
-                        <td>+1300277811</td>                       
-                        <td>Houston</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>Solution</td>
-                        <td>Jorg</td>
-                        <td>ancy@gmail.com</td>
-                        <td>www.solution.com</td>
-                        <td>+1233003487</td>
-                        <td>Philadelphia</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>4</td>
-                        <td>Teck</td>
-                        <td>Amy</td>
-                        <td>rocky@gmail.com</td>
-                        <td>www.teck.com</td>
-                        <td>+1800158813</td>
-                        <td>Phoenix</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>Dummy</td>
-                        <td>Semth</td>
-                        <td>johnsemth@gmail.com</td>
-                        <td>www.dummy.com</td>
-                        <td>+1300277811</td>                       
-                        <td>Houston</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>6</td>
-                        <td>Sample</td>
-                        <td>Jacky</td>
-                        <td>adomjacky@gmail.com</td>                       
-                        <td>www.sample.com</td>
-                        <td>+1334991276</td>
-                        <td>Chicago</td>
-                        <td>
-                          <a className="far fa-edit mr-4 text-success"></a>
-                          <a className="fas fa-trash-alt text-danger"></a>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default AllCompanies;
--- src/pages/auth/Forgetpassword.js
+++ src/pages/auth/Forgetpassword.js
@@ -1,55 +1,37 @@
 import React from "react";
 import "../../assets/css/forget-password.css";
 
-class ForgetPassword extends React.Component {
-  constructor(props) {
-    super(props);
+function ForgetPassword() {
 
-    this.state = {
-      data: [],
-      loading: false,
-    };
-  }
+  return (
+    <div>
+      <div className="forget-password">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8 col-lg-4">
+              <div className="card">
+                <h2>Forget Password</h2>
 
-  componentDidMount() {
-    console.log("ForgetPassword");
+                <form className="pt-5">
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
 
-    this.setState({ 
-      loading: true,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="forget-password">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-12 col-md-8 col-lg-4">
-                <div className="card">
-                  <h2>Forget Password</h2>
-
-                  <form className="pt-5">
-                    <div className="form-group">
-                      <label>Email</label>
-                      <input
-                        type="email"
-                        class="form-control"
-                        placeholder="Email"
-                      />
-                    </div>
-
-                    <div className="forget-btn">
-                      <a className="press d-block mt-4">Send</a>
-                    </div>
-                  </form>
-                </div>
+                  <div className="forget-btn">
+                    <a className="press d-block mt-4">Send</a>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default ForgetPassword;
